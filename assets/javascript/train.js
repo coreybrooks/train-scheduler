cb1420e14efb840044de1cf9c0634804c762ccab
Owner <Owner>
2017-03-08 12:44:08 -0600

    saving progress

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -35,16 +35,45 @@ database.push({});
  $('#add-train').on('click', function(event) {
  	event.preventDefault();
 
- 	var trainName = $('#train-name').val().trim();
- 	var destination = $('#destination').val().trim();
- 	var firstTrain = moment($('#first-train').val().trim(), 'HH:mm').format('HH:mm');
- 	var frequency = $('#frequency').val().trim();
+ 	var trainName = $('#name').val().trim();
+ 	var trainDestination = $('#destination').val().trim();
+ 	var trainFirst = moment($('#first').val().trim(), 'HH:mm').format('HH:mm');
+ 	var trainFrequency = $('#frequency').val().trim();
 
 var now = moment().format('HH:mm');
 
-console.log(firstTrain);
+console.log('train name is ' + trainName);
 console.log('current military time is ' + now);
 
+//create temp object to hold new train data
+var newTrain = {
+	name: trainName,
+	destination: trainDestination,
+	first: trainFirst,
+	frequency: trainFrequency  
+}
+
+//push new object to database
+database.ref().push(newTrain);
+//firebase.database().ref().push(newTrain);
+
+console.log(newTrain.name);
+console.log(newTrain.destination);
+console.log(newTrain.first);
+console.log(newTrain.frequency);
+
+//clear the text boxes
+ $('#name').val('');
+ $('#destination').val('');
+ $('#first').val('');
+ $('#frequency').val('');
+
+ })
+
+ //set up an event to append the data to the screen when a child is added to the database
+
+ database.ref().on('child_added', function(childSnapshot) {
+ 	console.log('added ' + childSnapshot.val());
 
  })
 
