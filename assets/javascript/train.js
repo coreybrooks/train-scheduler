c499d7ffd51c34c270616dcbb5837ee1919298ef
Owner <Owner>
2017-03-09 15:53:43 -0600

    schedule is working and so it the time validator

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,23 +1,4 @@
 
-/*
-currenttime-startTime = Total time since start
-
-Total time % whole route time = time currently taken
-
-moment().diff(moment(startTime), 'minutes')
-
-time left = whole route time-time currently taken
-
---------------------------------
-name = $('#name-input').val().trim();
-
-database.ref().set({name: name})
-
-database.push({});
-.on('child-added', function(snapshot) {
-	
-}*/
-
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDJP2AR7O4UOHMjOnUQTHFtBpV3s6gH3MU",
@@ -40,43 +21,80 @@ database.push({});
  	var trainFirst = moment($('#first').val().trim(), 'HH:mm').format('HH:mm');
  	var trainFrequency = $('#frequency').val().trim();
 
-var now = moment().format('HH:mm');
 
-console.log('train name is ' + trainName);
-console.log('current military time is ' + now);
+    // Varify if the time entered is valid, if not alert user and make them reenter
+    var validTest = moment(trainFirst, 'HH:mm').isValid();
 
-//create temp object to hold new train data
-var newTrain = {
-	name: trainName,
-	destination: trainDestination,
-	first: trainFirst,
-	frequency: trainFrequency  
-}
+ 	if (!validTest) {
+	  alert('that is not a valid time');
+      $('#first').val('');  	  //clear the time text box
+	  return;
+    }
+    
+    else  {
+    	console.log('valid time');
 
-//push new object to database
-database.ref().push(newTrain);
-//firebase.database().ref().push(newTrain);
+    //create temp object to hold new train data
+    var newTrain = {
+	    name: trainName,
+	    destination: trainDestination,
+	    first: trainFirst,
+	    frequency: trainFrequency  
+    }
 
-console.log(newTrain.name);
-console.log(newTrain.destination);
-console.log(newTrain.first);
-console.log(newTrain.frequency);
+    //push new object to database
+    database.ref().push(newTrain);
+    //firebase.database().ref().push(newTrain);
 
-//clear the text boxes
- $('#name').val('');
- $('#destination').val('');
- $('#first').val('');
- $('#frequency').val('');
 
- })
+    //clear the text boxes
+     $('#name').val('');
+     $('#destination').val('');
+     $('#first').val('');
+     $('#frequency').val('');
+    }
+ });
 
  //set up an event to append the data to the screen when a child is added to the database
 
  database.ref().on('child_added', function(childSnapshot) {
- 	console.log('added ' + childSnapshot.val());
+
+ 	//store everything into variables
+ 	var name = childSnapshot.val().name;
+ 	var destination = childSnapshot.val().destination;
+ 	var first = childSnapshot.val().first;
+ 	var frequency = childSnapshot.val().frequency;
+
+
+    var totalMinutes = moment().diff(moment(first, 'HH:mm'), 'minutes');
+
+    console.log('totalMinutes1 ' + totalMinutes);
+
+    var timeSinceLastArrival = totalMinutes % frequency;
+    var minutesAway = frequency - timeSinceLastArrival;
+    var nextArrival = moment().add(minutesAway, 'minutes').format('HH:mm');
+
+    if (totalMinutes < 0) {
+	    nextArrival = first;
+	    timeSinceLastArrival = '';
+	    minutesAway = (moment().diff(moment(first, 'HH:mm'), 'minutes'))*(-1) + ' *';
+    }
+
+
+    console.log('first ' + first);
+    console.log('totalMinutes ' + totalMinutes);
+    console.log('timeSinceLastArrival ' + timeSinceLastArrival);
+    console.log('frequency ' + frequency);
+    console.log('minutesAway ' + minutesAway);
+    console.log('nextArrival ' + nextArrival);
+    console.log('------------');
+
+
+ 	//append each train's data into the table
+ 	$('.display-table').append('<tr><td>' + name + '</td><td>' + destination + '</td><td>' + frequency + '</td><td>' + 
+ 	nextArrival + '</td><td>' + minutesAway + '</td></tr>');
 
  })
 
- //test changes to javascript in master branch
 
 
